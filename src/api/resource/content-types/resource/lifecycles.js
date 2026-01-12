5c5c742e3cb721dd611b6e4d867485a8387c961b
Elias <elrumo97@me.com>
2026-01-12 19:06:23 +0100

    Auto populare the feature_image field when featureImageFile is populated with a file

---
+++ src/api/resource/content-types/resource/lifecycles.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async beforeCreate(event) {
+    const { data } = event.params;
+
+    if (data.featureImageFile) {
+      await populateFeatureImageUrl(data, event);
+    }
+  },
+
+  async beforeUpdate(event) {
+    const { data } = event.params;
+
+    if (data.featureImageFile) {
+      await populateFeatureImageUrl(data, event);
+    }
+  },
+};
+
+async function populateFeatureImageUrl(data, event) {
+  let imageId = data.featureImageFile;
+
+  // Handle if featureImageFile is an object with id property
+  if (typeof imageId === 'object' && imageId !== null) {
+    imageId = imageId.id;
+  }
+
+  if (imageId) {
+    // Fetch the file details from Strapi's upload plugin
+    const file = await strapi.plugin('upload').service('upload').findOne(imageId);
+
+    if (file && file.url) {
+      // Set the feature_image field with the file URL
+      data.feature_image = file.url;
+    }
+  }
+}
